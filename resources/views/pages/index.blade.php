f49fe28ecb2569cdfa65c4adf7da0acb779e8978
gianlucanied <gianluca.nieddu96@gmail.com>
2024-02-12 15:49:42 +0100

    Add: stampa in pagina lista

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,16 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    <h1>Movies:</h1>
+    <ul>
+        @foreach ($movies as $movie)
+           <li>
+            {{ $movie -> title }}
+            {{ $movie -> original_title }}
+            {{ $movie -> nationality }}
+            {{ $movie -> date }}
+            {{ $movie -> vote }}
+           </li>
+        @endforeach
+    </ul>
 @endsection
